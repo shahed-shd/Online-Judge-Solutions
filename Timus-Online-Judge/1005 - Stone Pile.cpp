a5e2f53379c66d780cf700a7800220109e4850bb
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2015-06-12 02:18:49 +0600

    Timus 1005 - Stone Pile.cpp

--- Timus-Online-Judge/1005 - Stone Pile.cpp
+++ Timus-Online-Judge/1005 - Stone Pile.cpp
@@ -1,1 +1,41 @@
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define INT_MAX 2147483647
+
+int main()
+{
+    long int n, *w, i, pile1, pile2;
+
+    cin >> n;
+
+    w = new long int [n];
+
+    for(i = 0; i < n; i++) cin >> w[i];
+
+    if(n == 1) cout << w[0] << '\n';
+    else if(n == 2) cout << abs(w[0] - w[1]) << '\n';
+    else {
+        long int bits, mini;
+
+        mini = INT_MAX;
+
+        for(bits = (1L << n) - 1; bits >= 0; bits--) {
+            pile1 = pile2 = 0;
+
+            for(i = 0; i < n; i++) {
+                if(bits & (1L << i)) pile1 += w[i];
+                else pile2 += w[i];
+            }
+
+            if(abs(pile1 - pile2) < mini)
+                mini = abs(pile1-pile2);
+        }
+
+        cout << mini << '\n';
+    }
+
+    return 0;
+}
