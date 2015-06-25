e1f3e7e20e234fd04a8ccffd2da171d9bfc5f990
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2015-06-25 23:27:38 +0600

    UVa 10055 - Hashmat the Brave Warrior.cpp

---
+++ UVa-Online-Judge/10055 - Hashmat the Brave Warrior.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long long int a, b;
+
+    while(cin >> a >> b)
+        cout << abs(a-b) << '\n';
+
+    return 0;
+}
