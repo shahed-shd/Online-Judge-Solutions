85c56e52ad728c0b9e5c575253c86905f1fc40bf
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2015-12-27 02:41:16 +0600

    URI 1007 - Difference.cpp

---
+++ URI/1007 - Difference.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    printf("DIFERENCA = %d\n", a * b - c * d);
+
+    return 0;
+}
