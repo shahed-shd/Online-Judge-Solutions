9e361e865db28e2a8b11ea7d6bff208d2b29ec40
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-01-24 01:56:45 +0600

    UVa 280 - Vertex.cpp (Alternative solution added)

--- UVa-Online-Judge/280 - Vertex.cpp
+++ UVa-Online-Judge/280 - Vertex.cpp
@@ -1,3 +1,63 @@
+// Run time : 0.109 sec
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //freopen("in", "r", stdin);
+
+    int n;
+
+    while(scanf("%d", &n), n) {
+        bool mat[n][n];
+        fill_n(&mat[0][0], n*n, false);
+
+        int u, v;
+
+        while(scanf("%d", &u), u) {
+            --u;
+            while(scanf("%d", &v), v)
+                mat[u][--v] = true;
+        }
+
+        for(int k = 0; k < n; ++k)
+            for(int i = 0; i < n; ++i)
+                for(int j = 0; j < n; ++j)
+                    if(mat[i][k] && mat[k][j])
+                        mat[i][j] = true;
+
+        int s;
+        scanf("%d", &s);
+
+        while(s--) {
+            scanf("%d", &u);
+            --u;
+
+            vector<int> inaccessible;
+
+            for(int i = 0; i < n; ++i)
+                if(!mat[u][i])
+                    inaccessible.push_back(i+1);
+
+            printf("%u", inaccessible.size());
+
+            for(auto& x : inaccessible)
+                printf(" %d", x);
+
+            putchar('\n');
+        }
+    }
+
+    return 0;
+}
+
+
+// ------------------------ Alternatively ------------------------
+// Run time : 0.115 sec
+/*
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -20,6 +80,8 @@ void dfs(int u)
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    
     int n;
 
     while(cin >> n, n != 0) {
@@ -61,3 +123,5 @@ int main()
 
     return 0;
 }
+
+*/
