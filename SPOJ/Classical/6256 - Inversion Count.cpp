ba7b9add736f64f864652f19455d7de6e15092b7
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-03-31 23:55:37 +0600

    SPOJ 6256 - Inversion Count.cpp (Alternative solution added)

--- SPOJ/Classical/6256 - Inversion Count.cpp
+++ SPOJ/Classical/6256 - Inversion Count.cpp
@@ -1,3 +1,5 @@
+// Run time : 0.09 sec.
+
 #include <cstdio>
 using namespace std;
 
@@ -51,3 +53,72 @@ int main()
 
     return 0;
 }
+
+
+// ----------------------- Alternatively -----------------------
+// using Binary Indexed Tree (BIT).
+// Run time : 0.11 sec
+/*
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+class binIdxTree {
+    vector<int> tree;
+    int maxIdx;
+public:
+    binIdxTree(int n) { maxIdx = n; tree.assign(n+1, 0); }
+
+    void update(int idx, int val) {
+        while(idx <= maxIdx) {
+            tree[idx] += val;
+            idx += idx & (-idx);
+        }
+    }
+
+    int query(int idx) {
+        int total = 0;
+
+        while(idx > 0) {
+            total += tree[idx];
+            idx -= idx & (-idx);
+        }
+
+        return total;
+    }
+};
+
+int main()
+{
+    //freopen("in", "r", stdin);
+
+    int tc;
+    scanf("%d", &tc);
+
+    while(tc--) {
+        int n;
+        scanf("%d", &n);
+
+        int inp[n], mx = 0;
+
+        for(int i = 0; i < n; ++i) {
+            scanf("%d", inp+i);
+            mx = max(mx, inp[i]);
+        }
+
+        binIdxTree bit(mx);
+
+        long long ans = 0;
+
+        for(int i = n-1; i >= 0; --i) {
+            ans += bit.query(inp[i] - 1);
+            bit.update(inp[i], 1);
+        }
+
+        printf("%lld\n", ans);
+    }
+
+    return 0;
+}
+
+*/
