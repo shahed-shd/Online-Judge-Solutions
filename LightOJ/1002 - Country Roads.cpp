2eaeb1be9eacf281dfc5a7c5bcc2bde5d25d9f21
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-05-22 12:06:00 +0600

    LightOJ 1002 - Country Roads.cpp (Alternative solution added)

--- LightOJ/1002 - Country Roads.cpp
+++ LightOJ/1002 - Country Roads.cpp
@@ -1,3 +1,121 @@
+// Run time : 0.156 sec.
+
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+typedef     pair<int,int>   ii;
+typedef     pair<int,ii>    iii;
+
+const int MAXN = 500+3;
+
+vector<vector<ii> > adjList;
+int costs[MAXN];
+
+class UFDS {
+    int parent[MAXN], rnk[MAXN] = {0};
+public:
+    UFDS(int n) {
+        for(int i = 0; i < n; ++i)
+            parent[i] = i;
+    }
+
+    int findSet(int n) {
+        return ((parent[n] == n)? n : parent[n] = findSet(parent[n]));
+    }
+
+    bool unionSet(int u, int v) {
+        if(findSet(u) == findSet(v)) return false;
+
+        int p1 = findSet(u), p2 = findSet(v);
+        if(rnk[p1] > rnk[p2]) {
+            parent[p2] = p1;
+        }
+        else {
+            parent[p1] = p2;
+            if(rnk[p1] == rnk[p2]) ++rnk[p2];
+        }
+
+        return true;
+    }
+};
+
+void dfs(int u, int par = -1)
+{
+    int sz = adjList[u].size();
+
+    for(int i = 0; i < sz; ++i) {
+        int v = adjList[u][i].first;
+
+        if(v != par) {
+            costs[v] = max(costs[u], adjList[u][i].second);
+            dfs(v, u);
+        }
+    }
+}
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int T;
+    scanf("%d", &T);
+
+    for(int tc = 1; tc <= T; ++tc) {
+        adjList.clear();
+
+        int n, m;
+        scanf("%d %d", &n, &m);
+
+        vector<iii> edges;
+        int u, v, w, t;
+
+        while(m--) {
+            scanf("%d %d %d", &u, &v, &w);
+            if(u > v) swap(u, v);
+            edges.push_back(iii(w, ii(u, v)));
+        }
+        scanf("%d", &t);
+
+        sort(edges.begin(), edges.end());
+
+        vector<iii> spanningEdges;
+        UFDS uf(n);
+
+        for(vector<iii>::iterator it = edges.begin(); it != edges.end(); ++it) {
+            if(uf.unionSet(it->second.first, it->second.second)) {
+                spanningEdges.push_back(*it);
+                if(int(spanningEdges.size()) == n-1) break;
+            }
+        }
+
+        adjList.resize(n);
+
+        for(vector<iii>::iterator it = spanningEdges.begin(); it != spanningEdges.end(); ++it) {
+            w = it->first, u = it->second.first, v = it->second.second;
+            adjList[u].push_back(ii(v, w));
+            adjList[v].push_back(ii(u, w));
+        }
+
+        fill(costs, costs+n, -1);
+        costs[t] = 0;
+
+        dfs(t);
+
+        printf("Case %d:\n", tc);
+
+        for(int i = 0; i < n; ++i)
+            (costs[i] == -1)? puts("Impossible") : printf("%d\n", costs[i]);
+    }
+
+    return 0;
+}
+
+
+// ----------------------- Alternatively -----------------------
+// Run time : 0.208 sec.
+/*
 #include <cstdio>
 #include <algorithm>
 #include <queue>
@@ -73,3 +191,4 @@ int main()
 
     return 0;
 }
+*/
