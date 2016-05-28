8ee5735dc9add9dbfc9cb904835ffb395b0b11d1
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-05-28 19:43:51 +0600

    LightOJ 1019 - Brush (V).cpp (Alternative solution added)

--- LightOJ/1019 - Brush (V).cpp
+++ LightOJ/1019 - Brush (V).cpp
@@ -1,3 +1,4 @@
+// Run time : 0.016 sec.
 #include <cstdio>
 #include <queue>
 #include <vector>
@@ -67,3 +68,76 @@ int main()
 
     return 0;
 }
+
+
+// -------------------- Alternatively (with BFS) --------------------
+// Run time : 0.020 sec.
+/*
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <climits>
+using namespace std;
+
+typedef     pair<int,int>   ii;
+
+vector<vector<ii> > adjList;
+
+int shortestPath(int n)
+{
+    queue<int> q;
+    q.push(0);
+
+    int dist[n];
+    fill(dist, dist+n, INT_MAX);
+    dist[0] = 0;
+
+    while(!q.empty()) {
+        int u = q.front(); q.pop();
+
+        int sz = adjList[u].size();
+
+        for(int i = 0; i < sz; ++i) {
+            int v = adjList[u][i].first, w = adjList[u][i].second;
+
+            if(dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                q.push(v);
+            }
+        }
+    }
+
+    return dist[n-1];
+}
+
+int main()
+{
+    //freopen("in", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    for(int tc = 1; tc <= t; ++tc) {
+        adjList.clear();
+
+        int n, m, u, v, w;
+        scanf("%d %d", &n, &m);
+
+        adjList.resize(n);
+
+        while(m--) {
+            scanf("%d %d %d", &u, &v, &w);
+            adjList[--u].push_back(ii(--v, w));
+            adjList[v].push_back(ii(u, w));
+        }
+
+        int ans = shortestPath(n);
+
+        printf("Case %d: ", tc);
+
+        (ans == INT_MAX)? puts("Impossible") : printf("%d\n", ans);
+    }
+
+    return 0;
+}
+*/
