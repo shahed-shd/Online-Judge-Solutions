1e87c8597fbabc639472602e1bbdbf5a28f672b0
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-06-02 11:48:18 +0600

    LightOJ 1075 - Finding Routes.cpp (Alternative solution added)

--- LightOJ/1075 - Finding Routes.cpp
+++ LightOJ/1075 - Finding Routes.cpp
@@ -1,3 +1,4 @@
+// Run time : 0.016 sec.
 #include <cstdio>
 #include <map>
 #include <string>
@@ -67,3 +68,92 @@ int main()
 
     return 0;
 }
+
+// -------------------- Alternatively --------------------
+// Run time : 0.020 sec.
+/*
+#include <cstdio>
+#include <bitset>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef     pair<int,int>       ii;
+
+const int MAXS = 333+3;
+
+map<string, int> step_id;
+vector<string> id_step;
+int n;
+vector<vector<int> > adjList;
+bitset<MAXS> visited;
+vector<int> topoSort;
+
+int get_id(string str)
+{
+    if(step_id.find(str) == step_id.end()) {
+        step_id[str] = n++;
+        id_step.push_back(str);
+
+        return n-1;
+    }
+
+    return step_id[str];
+}
+
+void dfs(int u)
+{
+    visited[u] = true;
+
+    if(adjList[u].size()) {
+        int v = adjList[u][0];
+        if(!visited[v]) dfs(v);
+    }
+
+    topoSort.push_back(u);
+}
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    for(int tc = 1; tc <= t; ++tc) {
+        step_id.clear();
+        id_step.clear();
+        n = 0;
+        adjList.clear();
+        visited.reset();
+        topoSort.clear();
+
+        int s;
+        scanf("%d", &s);
+
+        adjList.resize(s);
+
+        char str1[100], str2[100];
+
+        for(int i = 1; i < s; ++i) {
+            scanf("%s %s", str1, str2);
+            int u = get_id(str1), v = get_id(str2);
+
+            adjList[u].push_back(v);
+        }
+
+        for(int i = 0; i < s; ++i)
+            if(!visited[i]) dfs(i);
+
+        printf("Case %d:\n", tc);
+
+        for(int i = s-1; i >= 0; --i)
+            puts(id_step[topoSort[i]].c_str());
+
+        putchar('\n');
+    }
+
+    return 0;
+}
+*/
