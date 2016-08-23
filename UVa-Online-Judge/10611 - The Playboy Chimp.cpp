63a25c27ecee8417e55177c67be0766eada388db
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-08-23 13:41:36 +0600

    Update 10611 - The Playboy Chimp.cpp

--- UVa-Online-Judge/10611 - The Playboy Chimp.cpp
+++ UVa-Online-Judge/10611 - The Playboy Chimp.cpp
@@ -43,3 +43,64 @@ int main()
 
     return 0;
 }
+
+
+// -------------------- Alternatively --------------------
+// Run time : 0.010 sec.
+/*
+#include <cstdio>
+using namespace std;
+
+const int MAXN = 50000 + 3;
+
+void binSearch(int arr[], int sz, int val, int &a, int &b)
+{
+    int s = 0, t = sz - 1;
+
+    while(s <= t) {
+        int mid = (s + t) >> 1;
+
+        if(arr[mid] == val) {
+            a = mid-1, b = mid+1;
+            return;
+        }
+
+        if(arr[mid] < val)
+            s = mid + 1;
+        else
+            t = mid - 1;
+    }
+
+    a = t, b = s;
+}
+
+int main()
+{
+    int n, chimps[MAXN];
+
+    while(scanf("%d", &n) != EOF) {
+        int h, sz = 0, last = -1;
+
+        while(n--) {
+            scanf("%d", &h);
+
+            if(h != last)
+                chimps[sz++] = last = h;   // Keeps only the unique elements.
+        }
+
+        int q, a, b;
+        scanf("%d", &q);
+
+        while(q--) {
+            scanf("%d", &h);
+
+            binSearch(chimps, sz, h, a, b);
+
+            (a >= 0)? printf("%d ", chimps[a]) : printf("X ");
+            (b < sz)? printf("%d\n", chimps[b]) : puts("X");
+        }
+    }
+
+    return 0;
+}
+*/
