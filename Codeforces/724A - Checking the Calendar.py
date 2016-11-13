8f8c3c3f341e9dc9d3b1dfea6a8a95d462c2ae0b
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2016-11-13 18:55:49 +0600

    Update 724A - Checking the Calendar.py

--- Codeforces/724A - Checking the Calendar.py
+++ Codeforces/724A - Checking the Calendar.py
@@ -7,7 +7,7 @@
 def main():
 	from sys import stdin, stdout
 
-	stdin = open("input.txt", "r")
+	# stdin = open("input.txt", "r")
 
 	dayNum = {"saturday" : 0, "sunday" : 1, "monday" : 2, "tuesday" : 3, "wednesday" : 4, "thursday" : 5, "friday" : 6}
 
