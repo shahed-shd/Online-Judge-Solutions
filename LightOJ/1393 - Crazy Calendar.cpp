8b096b4b0aa634f02b3845fad16ba2f36c522f52
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2017-01-18 06:20:28 +0600

    LightOJ 1393 - Crazy Calendar.cpp

--- LightOJ/1393 - Crazy Calendar.cpp
+++ LightOJ/1393 - Crazy Calendar.cpp
@@ -1,29 +1,44 @@
 // ==================================================
 // Problem  :   1393 - Crazy Calendar
-// Run time :   0.104 sec.
-// Language :   C++11
+// Run time :   0.092 sec.
+// Language :   C++
 // ==================================================
 
 #include <cstdio>
 using namespace std;
 
+template<class T>
+inline T fastIn()
+{
+    register char c = 0;
+    register T a = 0;
+    bool neg = false;
+
+    while(c < 33) c = getchar();
+
+    if(c == '-') neg = true;
+    else a = c - '0';
+
+    while(c = getchar(), c > 33)
+        a = a * 10 + (c - '0');
+
+    return (neg? -a : a);
+}
+
 int main()
 {
     //freopen("in.txt", "r", stdin);
 
-    int T;
-    scanf("%d", &T);
+    int T = fastIn<int>();
 
     for(int tc = 1; tc <= T; ++tc) {
-        int r, c;
-        scanf("%d %d", &r, &c);
+        int r = fastIn<int>(), c = fastIn<int>();
 
-        int stoneNum, lowerRight = r + c - 2;
-        int nim = 0;
+        int stoneNum, lowerRight = r+c-2, nim = 0;
 
         for(int i = 0; i < r; ++i) {
             for(int j = 0; j < c; ++j) {
-                scanf("%d", &stoneNum);
+                stoneNum = fastIn<int>();
 
                 if((lowerRight - (i+j)) & 1) nim ^= stoneNum;
             }
