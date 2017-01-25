e6e17477d3deabea85a8e5d20e8fc071c591495d
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2017-01-25 14:27:18 +0600

    LightOJ 1301 - Monitoring Processes.cpp (Alternative solution added)

--- LightOJ/1301 - Monitoring Processes.cpp
+++ LightOJ/1301 - Monitoring Processes.cpp
@@ -1,9 +1,58 @@
 // ==================================================
 // Problem  :   1301 - Monitoring Processes
-// Run time :   0.396 sec.
+// Run time :   0.288 sec.
 // Language :   C++
 // ==================================================
 
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+typedef     pair<int,int>       ii;
+
+vector<ii> V;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    for(int tc = 1; tc <= t; ++tc) {
+        V.clear();
+
+        int n;
+        scanf("%d", &n);
+
+        int si, ti;
+
+        for(int i = 0; i < n; ++i) {
+            scanf("%d %d", &si, &ti);
+
+            V.push_back(ii(si, 0));
+            V.push_back(ii(ti, 1));
+        }
+
+        sort(V.begin(), V.end());
+
+        int curr = 0, mx = 0, sz = V.size();
+
+        for(int i = 0; i < sz; ++i) {
+            curr += ((V[i].second)? -1 : 1);
+            mx = max(mx, curr);
+        }
+
+        printf("Case %d: %d\n", tc, mx);
+    }
+
+    return 0;
+}
+
+// -------------------- Alternatively --------------------
+// Run time :   0.396 sec.
+/*
 #include <cstdio>
 #include <algorithm>
 #include <set>
@@ -49,3 +98,4 @@ int main()
 
     return 0;
 }
+*/
