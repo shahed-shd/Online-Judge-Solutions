5eb9f453559bd9367bfc5696961223ae7cd8785a
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2017-01-25 22:35:38 +0600

    LightOJ 1301 - Monitoring Processes.cpp (Alternative solution added)

--- LightOJ/1301 - Monitoring Processes.cpp
+++ LightOJ/1301 - Monitoring Processes.cpp
@@ -50,7 +50,8 @@ int main()
     return 0;
 }
 
-// -------------------- Alternatively --------------------
+
+/// -------------------- Alternatively --------------------
 // Run time :   0.396 sec.
 /*
 #include <cstdio>
@@ -88,6 +89,65 @@ int main()
                 if(*mulSet.begin() < ts[i].first) {
                     mulSet.erase(mulSet.begin());
                 }
+            }
+
+            mulSet.insert(ts[i].second);
+        }
+
+        printf("Case %d: %lu\n", tc, mulSet.size());
+    }
+
+    return 0;
+}
+*/
+
+
+/// -------------------- Alternatively --------------------
+// Run time	: 0.436 sec.
+/*
+#include <cstdio>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+typedef     pair<int,int>       ii;
+
+const int MAXN = 50000 + 3;
+
+ii ts[MAXN];
+multiset<int> mulSet;
+
+bool cmpr(const ii &a, const ii &b)
+{
+    if(a.second != b.second)
+        return (a.second < b.second);
+    return (a.first < b.first);
+}
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    for(int tc = 1; tc <= t; ++tc) {
+        mulSet.clear();
+
+        int n;
+        scanf("%d", &n);
+
+        for(int i = 0; i < n; ++i)
+            scanf("%d %d", &ts[i].first, &ts[i].second);
+
+        sort(ts, ts+n, cmpr);       	// sort by second element of pair.
+		
+        multiset<int>::iterator it;
+
+        for(int i = 0; i < n; ++i) {
+            if(!mulSet.empty()) {
+                it = mulSet.lower_bound(ts[i].first);
+                if(it != mulSet.begin()) mulSet.erase(--it);
             }
 
             mulSet.insert(ts[i].second);
