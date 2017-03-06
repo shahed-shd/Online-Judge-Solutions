4a756f2996a432b3eab01d5fd73ba56cba135106
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2017-03-07 01:15:31 +0600

    Hacker Rank - sWAP cASE.py

---
+++ Hacker-Rank/Python/Strings/sWAP cASE.py
@@ -0,0 +1,8 @@
+# ==================================================
+# Problem	:	sWAP cASE
+# Score     :	10 /10
+# Language	:	Python 3
+# ==================================================
+
+def swap_case(s):
+    return s.swapcase()
