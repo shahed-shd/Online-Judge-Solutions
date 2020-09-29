b4fee587cfc5455c0b66f06038a28064cbe4582e
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2020-09-29 15:42:47 +0600

    Codeforces 703A - Mishka and Game.java

---
+++ Codeforces/703A - Mishka and Game.java
@@ -0,0 +1,120 @@
+// ==================================================
+// Problem  :   703A - Mishka and Game
+// Run time :   0.187 sec
+// Language :   Java
+// ==================================================
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        // FastScanner sc = new FastScanner("in.txt");
+        // PrintWriter out;
+        // try {
+        //     out = new PrintWriter("out.txt");
+        // } catch (FileNotFoundException e) {
+        //     e.printStackTrace();
+        //     return;
+        // }
+
+        FastScanner sc = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        solve(sc, out);
+        out.flush();
+        
+        sc.close();
+        out.close();
+    }
+
+    static void solve(FastScanner sc, PrintWriter out) {
+        int n = sc.nextInt();
+        int res = 0;
+ 
+        while (n-- > 0) {
+            int m = sc.nextInt();
+            int c = sc.nextInt();
+ 
+            if (m > c)
+                ++res;
+            else if (c > m)
+                --res;
+        }
+ 
+        if (res > 0) {
+            out.println("Mishka");
+        } else if (res < 0) {
+            out.println("Chris");
+        } else {
+            out.println("Friendship is magic!^^");
+        }
+    }
+}
+
+class FastScanner {
+    private BufferedReader br;
+    private StringTokenizer st;
+
+    public FastScanner(String fileName) {
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String readLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    public String nextToken() {
+        while (st == null || !st.hasMoreElements()) {
+            st = new StringTokenizer(readLine());
+        }
+        return st.nextToken();
+    }
+
+    public String nextLine() {
+        String str = "";
+
+        if (st != null && st.hasMoreElements()) {
+            str = st.nextToken("\n\r\f");
+        } else {
+            str = readLine();
+        }
+
+        return str;
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public void close() {
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
