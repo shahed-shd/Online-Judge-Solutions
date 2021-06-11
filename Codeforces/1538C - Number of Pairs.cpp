4c3e02e58002b226eedcc14cfb453cdaa528e4ad
Md. Shahedul Islam Shahed <shahed-shd@users.noreply.github.com>
2021-06-12 03:04:02 +0600

    Add alternative solution for Codeforces 1538C - Number of Pairs.cpp

--- Codeforces/1538C - Number of Pairs.cpp
+++ Codeforces/1538C - Number of Pairs.cpp
@@ -47,3 +47,68 @@ int main()
 
     return 0;
 }
+
+
+// Alternative solution, two pointers algorithm
+
+// ==================================================
+// Problem  :   1538C - Number of Pairs
+// Run time :   0.078 sec
+// Language :   C++17
+// ==================================================
+
+/*
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+
+typedef     long long       LL;
+
+
+const int MAXN = 2e5 + 3;
+
+int a[MAXN];
+
+
+LL countPairs(int n, int x)
+{
+    int i = 0, j = n - 1;
+    LL cnt = 0;
+
+    while(i < n) {
+        while(i < j && x <= a[i] + a[j]) {
+            --j;
+        }
+        cnt += n - 1 - max(i, j);
+        ++i;
+    }
+
+    return cnt;
+}
+
+
+int main()
+{
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n, l, r;
+        scanf("%d %d %d", &n, &l, &r);
+
+        for(int i = 0; i < n; ++i) scanf("%d", a+i);
+
+        sort(a, a+n);
+        LL ans = countPairs(n, l) - countPairs(n, r+1);
+
+        printf("%lld\n", ans);
+    }
+    
+
+    return 0;
+}
+*/
